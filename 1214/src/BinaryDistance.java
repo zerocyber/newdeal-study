1f6f043b053a8f0a38ff6d9cee98ffa4a74cace3
zerocyber <zeroforever@naver.com>
2018-12-14 17:58:11 +0900

    homework save

--- 1214/src/BinaryDistance.java
+++ 1214/src/BinaryDistance.java
@@ -1,18 +1,22 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BinaryDistance {
 
-	int soulution(int num) {
+	public int soulution(int num) {
 		List<Integer> list = new ArrayList<>();
 
 		int temp = num;
 
-
-
 		int result = list.size();
 		System.out.println("result : " + result);
-
+		
+		Collections.reverse(list); //순서 뒤집기
+		
+		System.out.println("list : " + list.toString());
+		
+		
 		return result;
 	}
 
@@ -22,14 +26,17 @@ public class BinaryDistance {
 
 		System.out.println("리스트의 크기: " + bd.soulution(10));*/
 		
-		int num = 10;
-		int temp = num;
-		
-		while(temp !=0) {
-			System.out.println("------");
-			temp =num / 2;
-			num = num % 2;
-			System.out.printf("a : %d  ,  temp : %d\n", num, temp);
+		int num = 10; //나눌 대상
+		int temp = 1; //나눈 나머지
+
+		while( temp == 0) {
+			System.out.println(".....");
+			temp = num % 2;
+			num = num /2;
+			
 		}
+		
+		
+		System.out.printf("num : %d , temp : %d\n", num, temp);
 	}
 }
