c9a3150ed985880d051c19b400bf5f2b5b26e10e
zerocyber <zeroforever@naver.com>
2018-12-19 12:15:40 +0900

    Spring-board 0.5

---
+++ bbsform/WebContent/board/writeFail.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+	pageEncoding="EUC-KR"%>
+<%
+String root = request.getContextPath();
+%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="ko">
+<head>
+<title>글입력 실패</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<link rel="stylesheet" href="<%=root%>/css/skin_purple.css" type="text/css">
+</head>
+
+<body>
+
+<table width="100%" cellpadding="6" cellspacing="2" border="0"
+	bgcolor="#ffffff" style="border: #e1e1e1 solid 1px">
+	<tr>
+		<td class="bg_board_title" width="100%"><img
+			src="<%=root%>/img/board/icon_arrow_08.gif" width="3" height="11"
+			border="0" align="absmiddle" hspace="6" vspace="6"> <b>게시판</b>
+		</td>
+	</tr>
+	<tr>
+		<td height="1" bgcolor="#e1e1e1"
+			style="overflow: hidden; padding: 0px;"></td>
+	</tr>
+	<tr>
+		<td class="bg_menu" width="100%" style="padding: 25px" height="35"
+			align="center"><b>게시물 등록이 실패 되었습니다.</b><br>
+		<br>
+
+		<div align="center"><a href=""><img
+			src="<%=root%>/img/board/btn_write_01.gif" width="91" height="21"
+			border="0" align="absmiddle" alt="다시 작성" hspace="10"></a><a href=""><img
+			src="<%=root%>/img/board/poll_listbu1.gif" width="62" height="21"
+			border="0" align="absmiddle" alt="목록보기" hspace="10"></a>
+		</td>
+	</tr>
+</table>
+<br>
+</body>
+</html>
---
+++ bbsform/WebContent/doc/PageNavigation.java
@@ -0,0 +1,134 @@
+package kr.co.java2u.util;
+
+public class PageNavigation {
+	private String root;
+	private boolean nowFirst;
+	private boolean nowEnd;
+	private int totalArticleCount;
+	private int newArticleCount;
+	private int totalPageCount;
+	private int pageNo;
+	private String navigator;
+
+	public void setRoot(String root) {
+		this.root = root;
+	}
+
+	public boolean isNowFirst() {
+		return nowFirst;
+	}
+
+	public void setNowFirst(boolean nowFirst) {
+		this.nowFirst = nowFirst;
+	}
+
+	public boolean isNowEnd() {
+		return nowEnd;
+	}
+
+	public void setNowEnd(boolean nowEnd) {
+		this.nowEnd = nowEnd;
+	}
+
+	public int getTotalArticleCount() {
+		return totalArticleCount;
+	}
+
+	public void setTotalArticleCount(int totalArticleCount) {
+		this.totalArticleCount = totalArticleCount;
+	}
+
+	public int getNewArticleCount() {
+		return newArticleCount;
+	}
+
+	public void setNewArticleCount(int newArticleCount) {
+		this.newArticleCount = newArticleCount;
+	}
+
+	public int getTotalPageCount() {
+		return totalPageCount;
+	}
+
+	public void setTotalPageCount(int totalPageCount) {
+		this.totalPageCount = totalPageCount;
+	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public String getNavigator() {
+		return navigator;
+	}
+
+	public void setNavigator() {
+		StringBuffer tmpNavigator = new StringBuffer();
+
+		tmpNavigator.append("<table cellpadding='0' cellspacing='0' border='0'>\n");
+		tmpNavigator.append(" <tr>\n");
+		if (this.isNowFirst()) {
+			tmpNavigator.append("  <td><font color='#999999'>\n<a href='javascript:goPage(1)'>");
+			tmpNavigator.append("   <img src='" + root + "/img/board/icon_prev02.gif' width='7' height='11' border='0' align='absmiddle' hspace='3'>최신목록</a>\n");
+			tmpNavigator.append("   <img src='" + root + "/img/board/icon_prev01_dim.gif' width='3' height='11' border='0' align='absmiddle' hspace='3'>\n");
+			tmpNavigator.append("   이전</font>\n");
+		} else {
+			tmpNavigator.append("  <td>\n<a href='javascript:goPage(1)'>");
+			tmpNavigator.append("   <img src='" + root + "/img/board/icon_prev02.gif' width='7' height='11' border='0' align='absmiddle' hspace='3'>최신목록 </a>\n");
+			tmpNavigator.append("   <a href='javascript:goPage(페이지계산)'>");
+			tmpNavigator.append("   <img src='" + root + "/img/board/icon_prev01_dim.gif' width='3' height='11' border='0' align='absmiddle' hspace='3'>\n");
+			tmpNavigator.append("   이전</a>");
+		}
+		tmpNavigator.append("  \n</td>\n");
+		tmpNavigator.append("  <td style='padding: 0 5 0 5'>\n");
+		tmpNavigator.append("   <table cellpadding='0' cellspacing='0' border='0'>\n");
+		tmpNavigator.append("    <tr>\n");
+		tmpNavigator.append("     <td width='1' nowrap><img src='" + root + "/img/board/n_tab.gif' width='1'");
+		tmpNavigator.append(" height='11' border='0' align='absmiddle'><br>");
+		tmpNavigator.append("     </td>\n");
+		
+		int startPage = 0;
+		int endPage = 0;
+		
+		for (int i = startPage; i <= endPage; i++) {
+			if (pageNo == i) {
+				tmpNavigator.append("     <td style='padding:0 7 0 7;' nowrap><font class='text_acc_02'><b>" + i + "</b></font></td>\n");
+				tmpNavigator.append("     <td width='1' nowrap><img src='" + root + "/img/board/n_tab.gif' width='1'");
+				tmpNavigator.append(" height='11' border='0' align='absmiddle'><br>\n");
+			} else {
+				tmpNavigator.append("     <td style='padding:0 7 0 7;' nowrap><a href='javascript:goPage(" + i + ")'>" + i + "</td>\n");
+				tmpNavigator.append("     <td width='1' nowrap><img src='" + root + "/img/board/n_tab.gif' width='1'");
+				tmpNavigator.append(" height='11' border='0' align='absmiddle'><br>\n");
+			}
+		}
+		tmpNavigator.append("     </td>\n");
+		tmpNavigator.append("    </tr>\n");
+		tmpNavigator.append("   </table>\n");
+		tmpNavigator.append("  </td>\n");
+		tmpNavigator.append("  <td>\n");
+		
+		if (this.isNowEnd()) {
+			tmpNavigator.append("   <font color='#999999'>다음<img");
+			tmpNavigator.append("   src='" + root + "/img/board/icon_next01_dim.gif' width='3' height='11'");
+			tmpNavigator.append(" border='0' align='absmiddle' hspace='3'> \n");
+			tmpNavigator.append("   끝목록<img src='" + root + "/img/board/icon_next02_dim.gif' width='7' height='11'");
+			tmpNavigator.append(" border='0' align='absmiddle' hspace='3'></font>\n");
+		} else {
+			tmpNavigator.append("   <a href='javascript:goPage(페이지계산)'>다음<img");
+			tmpNavigator.append(" src='" + root + "/img/board/icon_next01_dim.gif' width='3' height='11'");
+			tmpNavigator.append(" border='0' align='absmiddle' hspace='3'></a>\n");
+			tmpNavigator.append("   <a href='javascript:goPage(" + totalPageCount + ")'>끝목록<img src='" + root + "/img/board/icon_next02_dim.gif' width='7' height='11'");
+			tmpNavigator.append(" border='0' align='absmiddle' hspace='3'>\n");
+		}
+
+		tmpNavigator.append("  </td>\n");
+		tmpNavigator.append(" </tr>\n");
+		tmpNavigator.append("</table>\n");
+
+		this.navigator = tmpNavigator.toString();
+	}
+}
