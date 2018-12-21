b83e78200f8cc5bd3da6406de0ae1b314e1486ef
zerocyber <zeroforever@naver.com>
2018-12-21 09:25:15 +0900

    Spring-Board 2.1

--- boardframework/src/main/java/com/bit/board/service/ReboardServiceImpl.java
+++ boardframework/src/main/java/com/bit/board/service/ReboardServiceImpl.java
@@ -3,17 +3,23 @@ package com.bit.board.service;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bit.board.common.dao.CommonDao;
 import com.bit.board.model.ReboardDto;
 
 @Service
 public class ReboardServiceImpl implements ReboardService{
 
+	@Autowired
+	private SqlSession sqlSession;
+	
 	@Override
 	public int writeArticle(ReboardDto reboardDto) {
-		/*int seq = */
-		return 0;
+		int seq = sqlSession.getMapper(CommonDao.class).getNextSeq();
+		return seq;
 	}
 
 	@Override
